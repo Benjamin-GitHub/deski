d6000fc8a6e70f08a17282086daf60acd6a12412
Benjamin Mehrdad <75631604+Benjamin-GitHub@users.noreply.github.com>
2023-10-23 23:40:36 +0100

    Route installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './assets/logo.svg';
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import { HelmetMeta } from './HelmetMeta';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Switch>
+		<Route path="/" exact>
+      <HelmetMeta />
+			<Home />
+		</Route>
+		{/* <Route path="/about/this/site">
+			<About />
+		</Route> */}
+	</Switch>
+)
+  
+
 
 export default App;
